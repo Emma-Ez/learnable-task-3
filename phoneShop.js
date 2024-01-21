7446d4198827502f8750e95354e434aadafc7d84
Emma-Ez <emma.favoured@yahoo.com>
2024-01-21 22:18:02 +0100

    Display products function added

--- phoneShop.js
+++ phoneShop.js
@@ -3,3 +3,20 @@ const readline = require('readline').createInterface({
     output: process.stdout
   });
   
+// Array of Product catalogue
+const products = [
+    { id: 1, name: 'iPhone 15 Pro Max', price: 1800000 },
+    { id: 2, name: 'Samsung Galaxy S23', price: 1200000 },
+    { id: 3, name: 'Xiaomi 13T', price: 550000 }
+  ];
+
+ // Storing arrays of shopping cart
+let cart = [];
+
+ // Function to display the available products
+function displayProducts() {
+  console.log('');
+  products.forEach(product => {
+    console.log(`${product.id}. ${product.name} - ₦${product.price}`);
+  });
+}
